067d0e05d59e6b97b092826ac2a0cf7f21d84814
rksxox-coder <rksxox@gmail.com>
2025-11-18 21:48:25 +0530

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request
+from .utils import process_single_url, process_bulk_file
+
+main_blueprint = Blueprint("main", __name__)
+
+@main_blueprint.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@main_blueprint.route("/check", methods=["POST"])
+def check_url():
+    url = request.form.get("url")
+    result = process_single_url(url)
+    return render_template("result.html", result=result)
+
+@main_blueprint.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files.get("file")
+    results = process_bulk_file(file)
+    return render_template("result.html", results=results)
