f7c42846af60c303ce952b40ef7a1f675f707b8a
rksxox-coder <rksxox@gmail.com>
2025-11-19 00:02:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from app.routes import main_blueprint
 
+app.secret_key = "replace_with_a_random_key"
+app.permanent_session_lifetime = timedelta(minutes=30)
+
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main_blueprint)
